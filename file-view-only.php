37deaf098633b1d82cb824b1637639558042cb48
Kavinkumar B <97976387+Theternos@users.noreply.github.com>
2023-03-07 16:31:09 +0530

    Added features to the pdf VIew page

--- file-view-only.php
+++ file-view-only.php
@@ -69,6 +69,55 @@ $sector_filter = $_POST['sector'];
     #element {
         display: block;
     }
+
+    #canvas_container {
+        text-align: center;
+        margin-top: 2vh;
+    }
+
+    .pdf-controls {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .pdf-controls button {
+        width: fit-content;
+        padding: 5px;
+        height: 30px;
+        background-color: #fff;
+        color: #000;
+        margin: 5px;
+        border-color: 0px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    body {
+        position: relative;
+    }
+
+    body::after {
+        content: "<?php echo $username ?>";
+        font-size: 5em;
+        font-weight: bold;
+        color: #ccc;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(-45deg);
+        pointer-events: none;
+        z-index: 2;
+        opacity: 0.2;
+    }
+
+    @media screen and (max-width: 960px) {
+        body::after {
+            top: 65%;
+            font-size: 2em;
+        }
+    }
 </style>
 
 <body class="noprint" oncontextmenu="return false;">
@@ -139,10 +188,19 @@ $sector_filter = $_POST['sector'];
                 </div>
         <?php }
         } ?>
-
-        <div class="wrapper">
-            <iframe style="margin-top: 2vh;" src="<?php echo $file ?>#toolbar=0&amp;navpanes=0&amp;statusbar=0&readonly=true; disableprint=true;" width="100%" height="1500" frameborder="0"></iframe>
-            <div class="embed-cover"></div>
+        <div id="my_pdf_viewer">
+            <div id="canvas_container">
+                <canvas id="pdf_renderer"></canvas>
+            </div>
+            <div class="pdf-controls" id="navigation_controls">
+                <button id="go_previous">Previous</button>
+                <button id="go_next">Next</button>
+                <div id="zoom_controls">
+                    <button id="zoom_in">Zoom In</button>
+                    <button id="zoom_out">Zoom Out</button>
+                </div>
+                <input id="current_page" value="1" type="hidden" />
+            </div>
         </div>
     </div>
 
@@ -282,5 +340,88 @@ $sector_filter = $_POST['sector'];
     <script type="text/javascript" src="https://pdfanticopy.com/noprint.js"></script>
 
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.min.js"></script>
+<script>
+    var myState = {
+        pdf: null,
+        currentPage: 1,
+        zoom: 1.2
+    }
+
+    pdfjsLib.getDocument('<?php echo $file ?>').then((pdf) => {
+
+        myState.pdf = pdf;
+        render();
+
+    });
+
+    function render() {
+        myState.pdf.getPage(myState.currentPage).then((page) => {
+
+            var canvas = document.getElementById("pdf_renderer");
+            var ctx = canvas.getContext('2d');
+
+            var viewport = page.getViewport(myState.zoom);
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            page.render({
+                canvasContext: ctx,
+                viewport: viewport
+            });
+        });
+    }
+
+    document.getElementById('go_previous')
+        .addEventListener('click', (e) => {
+            if (myState.pdf == null || myState.currentPage == 1)
+                return;
+
+            myState.currentPage -= 1;
+            document.getElementById("current_page").value = myState.currentPage;
+            render();
+        });
+    document.getElementById('go_next')
+        .addEventListener('click', (e) => {
+            if (myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages)
+                return;
+
+            myState.currentPage += 1;
+            document.getElementById("current_page").value = myState.currentPage;
+            render();
+        });
+    document.getElementById('current_page')
+        .addEventListener('keypress', (e) => {
+            if (myState.pdf == null) return;
+
+            // Get key code
+            var code = (e.keyCode ? e.keyCode : e.which);
+
+            // If key code matches that of the Enter key
+            if (code == 13) {
+                var desiredPage = document.getElementById('current_page').valueAsNumber;
+
+                if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+                    myState.currentPage = desiredPage;
+                    document.getElementById("current_page").value = desiredPage;
+                    render();
+                }
+            }
+        });
+    document.getElementById('zoom_in')
+        .addEventListener('click', (e) => {
+            if (myState.pdf == null) return;
+            myState.zoom += 0.5;
+            render();
+        });
+    document.getElementById('zoom_out')
+        .addEventListener('click', (e) => {
+            if (myState.pdf == null) return;
+            myState.zoom -= 0.5;
+
+            render();
+        });
+</script>
+
+
 
 </html>
